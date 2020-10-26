9c639db9504c2f12079157669956c80fd72747d4
AndreeaIsabela <bala.andreea@yahoo.com>
2020-10-26 17:33:36 +0200

    [metrici-software] Add teacher routes class.

--- server/routes/teacher.ts
+++ server/routes/teacher.ts
@@ -1,127 +1,26 @@
 import { Router } from "express";
-const teacherRoutes: any = Router();
-import { TeacherController } from "../controllers/teacher";
-import { TeacherModel as TeacherModel } from "../models/teacher";
+import { TeacherRoutes } from './teacherRoutes';
 
 const jwtService = require('../middleware/authenticationMiddleware');
+const teacherRoutes = new TeacherRoutes();
+const router: any = Router();
 
-// injecting the teacher model in the controller instance
-const teacherController = new TeacherController(TeacherModel);
+router.post('/login', teacherRoutes.login);
 
-teacherRoutes.post('/login', async (req, res) => {
-  try {
-    const teacher = await teacherController.getTeacher(req.body.email);
+router.get('/', jwtService.teacherAuthentication, teacherRoutes.getTeachers);
 
-    if (teacher.comparePassword(req.body.password) === true) {
-      const teacherJson = teacher.toJSON();
-      res.send({
-        user: teacherJson,
-        token: teacherController.jwtSignUser(teacherJson),
-        userId: teacherJson._id
-      });
-      return res.status(200).end();
-    }
-    return res.status(401).end();
-  }
-  catch (err) {
-    console.error(err);
-    return res.status(500).end();
-  }
-});
+router.get('/:id', jwtService.teacherAuthentication, teacherRoutes.getTeacherById);
 
-teacherRoutes.get('/', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    const teachers = await teacherController.getTeachers();
-    res.json(teachers);
-  } catch (err) {
-    return res.status(500).end();
-  }
-});
+router.post('/register', teacherRoutes.registerTeacher);
 
-teacherRoutes.get('/:id', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    const teacher = await teacherController.getTeacherById(req.params.id);
-    res.json(teacher);
-  }
-  catch (err) {
-    console.error(err);
-    return res.status(500).end();
-  }
-});
+router.post('/:teacherId/resources', teacherRoutes.addLearningResource);
 
-teacherRoutes.post('/register', async (req, res) => {
-  try {
-    const teacher = await teacherController.addTeacher(req.body);
-    res.json(teacher);
-    return res.status(200).end();
-  } catch (err) {
-    return res.status(500).end();
-  }
-});
+router.put('/:id', jwtService.teacherAuthentication, teacherRoutes.updateTeacher);
 
-teacherRoutes.post('/:teacherId/resources', async (req, res) => {
-  try {
-    await teacherController.addResource(req.body, req.params.teacherId);
-    return res.status(200).end();
-  } catch (err) {
-    return res.status(500).end();
-  }
-});
+router.get('/:teacherId/resources/totalPages', jwtService.teacherAuthentication, teacherRoutes.getTotalNoOfResources);
 
-teacherRoutes.put('/:id', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    const updatedTeacher = await teacherController.updateTeacher(req.params.id, req.body);
-    const updatedTeacherJson: string = updatedTeacher.toJSON();
-    res.send({
-      user: updatedTeacherJson,
-      token: teacherController.jwtSignUser(updatedTeacherJson)
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).end();
-  }
-});
+router.get('/:teacherId/resources/page/:page', jwtService.teacherAuthentication, teacherRoutes.getResourcesPerPage);
 
-teacherRoutes.get('/:teacherId/resources/totalPages', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    //get the total number of items used for pagination
-    const teacherId = req.params.teacherId;
-    const response = await teacherController.getTotaNrOfLinks(teacherId);
-    return res.json(Number(response));
-  } catch (err) {
-    res.status(500).end();
-  }
-});
+router.delete('/:teacherId/resources/:resourceId', jwtService.teacherAuthentication, teacherRoutes.deleteResource);
 
-teacherRoutes.get('/:teacherId/resources/page/:page', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    const teacherId = req.params.teacherId;
-    const pageNo = parseInt(req.params.page);
-    const size = 20;
-    var query: any = {};
-    if (pageNo < 0 || pageNo === 0) {
-      const response = { "error": true, "message": "invalid page number, should start with 1" };
-      return res.json(response);
-    }
-    query.skip = size * (pageNo - 1);
-    query.limit = size;
-    const links = await teacherController.getPageLinks(query, teacherId);
-    res.json(links);
-  } catch (err) {
-    console.log(err);
-    res.status(500).end();
-  }
-});
-
-teacherRoutes.delete('/:teacherId/resources/:resourceId', jwtService.teacherAuthentication, async (req, res) => {
-  try {
-    const teacherId = req.params.teacherId;
-    await teacherController.deleteResource(teacherId, req.params.resourceId);
-    res.status(204).end();
-  } catch (err) {
-    console.log(err);
-    res.status(500).end();
-  }
-});
-
-module.exports = teacherRoutes;
+module.exports = router;
---
+++ server/routes/teacherRoutes.ts
@@ -0,0 +1,126 @@
+import { TeacherController } from "../controllers/teacher";
+import { TeacherModel as TeacherModel } from "../models/teacher";
+
+// injecting the teacher model in the controller instance
+const teacherController = new TeacherController(TeacherModel);
+
+export class TeacherRoutes {
+
+  constructor() { }
+
+  readonly login = async (req, res, _next) => {
+    try {
+      const teacher = await teacherController.getTeacher(req.body.email);
+
+      if (teacher.comparePassword(req.body.password) === true) {
+        const teacherJson = teacher.toJSON();
+        res.send({
+          user: teacherJson,
+          token: teacherController.jwtSignUser(teacherJson),
+          userId: teacherJson._id
+        });
+        return res.status(200).end();
+      }
+      return res.status(401).end();
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+  }
+
+  readonly getTeachers = async (req, res, _next) => {
+    try {
+      const teachers = await teacherController.getTeachers();
+      res.json(teachers);
+    } catch (err) {
+      return res.status(500).end();
+    }
+  }
+
+  readonly getTeacherById = async (req, res, _next) => {
+    try {
+      const teacher = await teacherController.getTeacherById(req.params.id);
+      res.json(teacher);
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+  }
+
+  readonly registerTeacher = async (req, res, _next) => {
+    try {
+      const teacher = await teacherController.addTeacher(req.body);
+      res.json(teacher);
+      return res.status(200).end();
+    } catch (err) {
+      return res.status(500).end();
+    }
+  }
+
+  readonly addLearningResource = async (req, res, _next) => {
+    try {
+      await teacherController.addResource(req.body, req.params.teacherId);
+      return res.status(200).end();
+    } catch (err) {
+      return res.status(500).end();
+    }
+  }
+
+  readonly updateTeacher = async (req, res, _next) => {
+    try {
+      const updatedTeacher = await teacherController.updateTeacher(req.params.id, req.body);
+      const updatedTeacherJson: string = updatedTeacher.toJSON();
+      res.send({
+        user: updatedTeacherJson,
+        token: teacherController.jwtSignUser(updatedTeacherJson)
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+  }
+
+  readonly getTotalNoOfResources = async (req, res, _next) => {
+    try {
+      //get the total number of items used for pagination
+      const teacherId = req.params.teacherId;
+      const response = await teacherController.getTotaNrOfLinks(teacherId);
+      return res.json(Number(response));
+    } catch (err) {
+      res.status(500).end();
+    }
+  }
+
+  readonly getResourcesPerPage = async (req, res, _next) => {
+    try {
+      const teacherId = req.params.teacherId;
+      const pageNo = parseInt(req.params.page);
+      const size = 20;
+      var query: any = {};
+      if (pageNo < 0 || pageNo === 0) {
+        const response = { "error": true, "message": "invalid page number, should start with 1" };
+        return res.json(response);
+      }
+      query.skip = size * (pageNo - 1);
+      query.limit = size;
+      const links = await teacherController.getPageLinks(query, teacherId);
+      res.json(links);
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+  }
+
+  readonly deleteResource = async (req, res, _next) => {
+    try {
+      const teacherId = req.params.teacherId;
+      await teacherController.deleteResource(teacherId, req.params.resourceId);
+      res.status(204).end();
+    } catch (err) {
+      console.log(err);
+      res.status(500).end();
+    }
+  }
+}
